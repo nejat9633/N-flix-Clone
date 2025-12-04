6f02884b767838e48437bf88fbb560803d8381cc
NJ <nej9633@gmail.com>
2025-12-04 12:02:30 +0300

    responsive

--- netflix-clone/src/components/Header/Header.jsx
+++ netflix-clone/src/components/Header/Header.jsx
@@ -5,10 +5,13 @@ import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import AccountBoxIcon from "@mui/icons-material/AccountBox";
 import style from './Header.module.css'
 import logo from '../../assets/logo.png'
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
 
 function Header() {
 
   const [show, setShow] = useState(false);
+  const [open,setOpen] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -20,13 +23,14 @@ function Header() {
   return (
     <>
       <nav
-        className={style.navbar}
         style={{
           backgroundColor: show ? "rgb(17,17,17)" : "transparent",
+        
         }}
+        className={`${style.navbar} ${open ? style.navbarOpen : ""}`}
       >
         <div className={style.leftNavContainer}>
-          <ul className={style.links}>
+          <ul className={`${style.links} ${open ? style.active : ""} `}>
             <li>
               <img className={style.logo} src={logo} alt="Netflix Logo" />
             </li>
@@ -37,6 +41,14 @@ function Header() {
             <li>My List</li>
             <li>Browse by Language</li>
           </ul>
+        </div>
+
+        <div className={style.menu} onClick={() => setOpen(!open)}>
+          {open ? (
+            <CloseIcon style={{ fontSize: 28 }} />
+          ) : (
+            <MenuIcon style={{ fontSize: 28 }} />
+          )}
         </div>
 
         <div className={style.rightNavContainer}>
